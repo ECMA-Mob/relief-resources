32ecc972974c4068a5c1d39d59ddf8de9756c4c8
Henry Quinn <henryquinniv@gmail.com>
2020-03-22 15:03:05 -0400

    First commit.

---
+++ src/components/description.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Description = ({ city }) => (
+  <p className="text-lg mb-8">
+    A directory of resources for {city} residents, businesses, and organizations that can use some help to support both the community and their staff during (and beyond) the state-mandated shutdown. The best way we can help our neighbors is by supporting the organizations that support them. Please consider donating to some of the organizations listed below. A work-in-progress.
+  </p>
+);
+
+export default Description;
